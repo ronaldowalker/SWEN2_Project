0eae3271043e09c150c03fbf380c98d6d3ab8130
Sanjeev Praboocharan <sanjeevpraboo@gmail.com>
2024-02-07 19:40:35 +0000

    karma

---
+++ App/models/karma.py
@@ -0,0 +1,21 @@
+from App.database import db
+from .student import Student
+from datetime import datetime
+
+class Karma(db.Model):
+  __tablename__ = "karma"
+  karmaID = db.Column(db.Integer, primary_key=True)
+  points = db.Column(db.Float, nullable=False, default=0.0)
+  rank = db.Column(db.Integer, nullable=False, default=-99)
+  studentID = db.Column(db.String(10), db.ForeignKey('student.ID'))
+  associatedStudent = db.relationship('Student', backref=db.backref('karma', lazy='joined'),foreign_keys=[studentID])
+
+  def __init__(self,ID,student, studentID ,points=0.0, rank=-99):
+    self.karmaID = ID
+    self.points = points
+    self.associatedStudent = student
+    self.rank = rank
+    self.studentID = studentID
+
+  def to_json(self):
+    return {"karmaID": self.karmaID, "score": self.points, "rank": self.rank}
