20ed9e115e943355d0e5555d624a5b7f807ada7d
RichardR963 <richardrattansingh21@gmail.com>
2024-02-11 21:03:35 +0000

    added controllers

---
+++ App/controllers/karma.py
@@ -0,0 +1,35 @@
+from App.models import Karma
+from App.database import db 
+from .review import (
+    get_total_points
+)
+
+def create_karma(student):
+    newKarma = Karma(student, student.ID, points=0.0, rank=-99)
+    db.session.add(newKarma)
+    try:
+        db.session.commit()
+        return True
+    except Exception as e:
+        print("[karma.create_karma] Error occurred while creating new karma: ", str(e))
+        return False
+
+def calculate_karma(student):
+    karma = get_total_points(student.ID)
+    return karma
+
+def update_karma(student):
+    karmaScore = Karma.query.filter_by(studentID=student.ID).first()
+    if karmaScore:
+        karmaScore.points = calculate_karma(student)
+        try:
+            db.session.commit()
+            return True
+        except Exception as e:
+            print("[karma.update_karma] Error occurred while updating karma:", str(e))
+            db.session.rollback()
+            return False
+    else:
+        print("Karma score not found for student:", student.ID)
+        return False
+
--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,20 +1,38 @@
 from App.models import User
 from App.database import db
 
-def create_user(username, password):
-    newuser = User(username=username, password=password)
+def create_user(username, firstname, lastname, password, email, faculty):
+    newuser = User(username=username, firstname=firstname ,lastname=lastname, password=password, email=email, faculty=faculty)
     db.session.add(newuser)
-    db.session.commit()
-    return newuser
+    try:
+        db.session.commit()
+        return newuser
+    except Exception as e:
+        print("[user.create_user] Error occurred while creating new user: ", str(e))
+        db.session.rollback()
+        return None
+    
 
 def get_user_by_username(username):
-    return User.query.filter_by(username=username).first()
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return user
+    else:
+        return None
 
 def get_user(id):
-    return User.query.get(id)
+    user = User.query.get(id)
+    if user:
+        return user
+    else:
+        return None
 
 def get_all_users():
-    return User.query.all()
+    users = User.query.all()
+    if users:
+        return users
+    else:
+        return []
 
 def get_all_users_json():
     users = User.query.all()
@@ -23,11 +41,17 @@ def get_all_users_json():
     users = [user.get_json() for user in users]
     return users
 
-def update_user(id, username):
+def update_user_username(id, username):
     user = get_user(id)
     if user:
         user.username = username
         db.session.add(user)
-        return db.session.commit()
-    return None
+        try:
+            db.session.commit()
+            return True
+        except Exception as e:
+            print("[user.update_user_username] Error occurred while creating new user: ", str(e))
+            db.session.rollback()
+            return False
+    return False
     
