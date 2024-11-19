817a202346523eecf0751ff33260d595c7dbd05a
ronaldowalker <ronaldo.walker@my.uwi.edu>
2024-11-19 00:29:13 -0400

    Update user model

--- App/models/user.py
+++ App/models/user.py
@@ -10,20 +10,11 @@ class User(db.Model, UserMixin):
     lastname = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), nullable=False)
-    faculty = db.Column(db.String(120), nullable=False)
-    user_type = db.Column(db.String(20), nullable=False)
-    
-    __mapper_args__ = {
-        "polymorphic_identity": "user",
-        "polymorphic_on": user_type,
-    }
 
-    def __init__(self, username, firstname,lastname , password, email, faculty):
+    def __init__(self, username, firstname,lastname , password):
         self.username= username
         self.firstname = firstname
         self.lastname = lastname
-        self.email = email
-        self.faculty = faculty
         self.set_password(password)
 
     def get_json(self):
@@ -33,7 +24,6 @@ class User(db.Model, UserMixin):
             'firstname': self.firstname,
             'lastname': self.lastname,
             'email': self.email,
-            'faculty': self.faculty
         }
     
     def get_id(self):
