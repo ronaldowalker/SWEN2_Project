cdda19e10cb445952da56bf394396d5e7e48d860
ronaldowalker <ronaldo.walker@my.uwi.edu>
2024-11-21 10:42:58 -0400

    Staff model update

--- App/models/staff.py
+++ App/models/staff.py
@@ -7,25 +7,17 @@ class Staff(User):
   __tablename__ = 'staff'
   ID = db.Column(db.Integer, db.ForeignKey('user.ID'), primary_key=True)
   reviews = db.relationship('Review', backref='staffReviews', lazy='joined')
-  reports = db.relationship('IncidentReport',
-                            backref='staffReports',
-                            lazy='joined')
-  pendingAccomplishments = db.relationship('Accomplishment',
-                                           backref='studentaccomplishments',
-                                           lazy='joined')
 
   __mapper_args__ = {"polymorphic_identity": "staff"}
 
-  def __init__(self, username, firstname, lastname, email, password, faculty):
+  def __init__(self, username, firstname, lastname, email, password):
     super().__init__(username=username,
                      firstname=firstname,
                      lastname=lastname,
                      email=email,
-                     password=password,
-                     faculty=faculty)
+                     password=password)
     self.reviews = []
-    self.reports = []
-    self.pendingAccomplishments = []
+
 
 
 #return staff details on json format
@@ -42,14 +34,7 @@ class Staff(User):
         self.lastname,
         "email":
         self.email,
-        "faculty":
-        self.faculty,
         "reviews": [review.to_json() for review in self.reviews],
-        "reports": [report.to_json() for report in self.reports],
-        "pendingAccomplishments": [
-            pendingAccomplishment.to_json()
-            for pendingAccomplishment in self.pendingAccomplishments
-        ]
     }
 
   def __repr__(self):
