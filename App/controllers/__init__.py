b92ff2cbd1e74522a0cf926953de4cf471878b4e
ZAllaham <157640657+ZAllaham@users.noreply.github.com>
2024-11-27 12:27:39 -0400

    Commit

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -3,17 +3,7 @@ from .auth import *
 from .staff import *
 from .student import *
 from .karma import *
-from .accomplishment import *
 from .review import *
-from .recommendation import *
-from .incidentReport import *
-from .grades import *
-from .admin import *
 from .csv import *
 from .staffcsv import *
-from .transcript import *
 from .nltk import *
-from .schoolRecommendation import *
-from .jobRecommendation import *
-from .badges import *
-
