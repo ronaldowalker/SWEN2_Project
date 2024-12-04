02fc396cc966ad2a4bf4904a65a83753c3c17946
Ronaldo Walker <ronaldowalker10295@gmail.com>
2024-12-04 17:28:34 +0000

    karma Unit and Integration tests

--- App/tests/test_karma.py
+++ App/tests/test_karma.py
@@ -5,10 +5,7 @@ from App.main import create_app
 from App.database import db, create_db
 from App.models import Karma
 from App.controllers import (
-    get_karma,
-    create_karma,
-    calculate_review_points,
-    update_review_points
+    to_json
 )
 
 '''
@@ -17,22 +14,27 @@ from App.controllers import (
 class KarmaUnitTests(unittest.TestCase):
 
     def test_new_karma(self):
-        newKarma = Karma(points=0.0, academicPoints=0.0, accomplishmentPoints=0.0, reviewsPoints=0.0,incidentPoints=0.0, rank=-99, studentID = 1)
-        assert newKarma is not None
+        assert create_student(studentID=816036000, firstname = "sly", lastname="cooper", karma=0) == True
+        student = get_student_by_studnetID(816036000)
+        history = Karma(studentID = student.ID, action = "increase", amount = 1, previous_karma = 0, updated_karma = 1)
+        assert history is not None
+        
 
     def test_karma_to_json(self):
-        newKarma = Karma(points=0.0, academicPoints=0.0, accomplishmentPoints=0.0, reviewsPoints=0.0,incidentPoints=0.0, rank=-99, studentID = 1)
-        newKarma_json = newKarma.to_json()
-        self.assertDictEqual(newKarma_json,{
-            "karmaID": None,
-            "score": 0.0,
-            "academicPoints": 0.0, 
-            "accomplishmentPoints": 0.0, 
-            "reviewPoints": 0.0,
-            "incidentPoints": 0.0,
-            "rank": -99,
-            "studentID": 1
+        assert create_student(studentID=816036000, firstname = "sly", lastname="cooper", karma=0) == True
+        student = get_student_by_studnetID(816036000)
+        history = Karma(studentID = student.ID, action = "increase", amount = 1, previous_karma = 0, updated_karma = 1)
+        history_json = history.to_json()
+        self.assertDictEqual(history_json,{
+            "id": 1,
+            "student_id": f"{student.ID}",
+            "action": "increase", 
+            "amount": "1", 
+            "previous_karma": "0",
+            "updated_karma": "1",
+            "timestamp": -f"{history.timestamp}",
         })
+
 '''
     Integration Tests
 '''
@@ -47,18 +49,3 @@ def empty_db():
     db.drop_all()
 
 class KarmaIntegrationTests(unittest.TestCase):
-
-    def test_create_karma(self):
-        assert create_karma(1) == True
-
-    def test_get_karma(self):
-        self.test_create_karma()
-        karma = get_karma(1)
-        assert karma.studentID == 1
-
-    def test_calculate_karma(self):
-        karma = calculate_review_points(1)
-        assert karma == 0
-
-    def test_update_karma(self):
-        assert update_review_points(1) == True
